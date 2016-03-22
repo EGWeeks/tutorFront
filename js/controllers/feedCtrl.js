f396a9e8a095530d389851de173e03566d9cda91
Ethan Weeks <ethanweeks@ymail.com>
2016-03-22 11:37:55 -0600

    updated map logic to feedCtrl

--- js/controllers/feedCtrl.js
+++ js/controllers/feedCtrl.js
@@ -12,6 +12,18 @@ angular.module('feedCtrl', [])
 	  		.then(function(response){
 	  			console.log(response.data.posts);
 	  			vm.postings = response.data.posts;
+	  			//Create Map
+	  			L.mapbox.accessToken = 'pk.eyJ1Ijoiam1sYWJzIiwiYSI6Imlnc1pXbncifQ.1U4VwxWkGS_Y3TpZ6-sf4A';
+
+	  			var map = L.mapbox.map('map', 'jmlabs.k3egm800', {
+    									center: [40.55, -105.07], // lat, long
+    									zoom: 13
+									  });
+
+									  L.mapbox.tileLayer('mapbox.streets', {
+									      maxZoom: 18
+									  }).addTo(map);
+									  console.log('MAP!!');
 	  		})
 	  		.catch(function(err){
 	  			console.log(err);
--- js/map/main.js
+++ js/map/main.js
@@ -1,40 +1,40 @@
-$(document).ready(function() {
-  /*
-   * Application Variables
-   */
-  // Access token
-  L.mapbox.accessToken = 'pk.eyJ1Ijoiam1sYWJzIiwiYSI6Imlnc1pXbncifQ.1U4VwxWkGS_Y3TpZ6-sf4A';
+// $(document).ready(function() {
+//   /*
+//    * Application Variables
+//    */
+//   // Access token
+//   L.mapbox.accessToken = 'pk.eyJ1Ijoiam1sYWJzIiwiYSI6Imlnc1pXbncifQ.1U4VwxWkGS_Y3TpZ6-sf4A';
 
-  // Define Rob Lowe Icon
-  // var robLoweIcon = L.icon({
-  //   // Specify a class name we can refer to in CSS.
-  //   className: 'rob-lowe-icon',
-  //   // Set marker width and height
-  //   iconSize: [120, 181],
-  //   // Set icon url
-  //   iconUrl: 'assets/images/rob-lowe.png'
-  // });
+//   // Define Rob Lowe Icon
+//   // var robLoweIcon = L.icon({
+//   //   // Specify a class name we can refer to in CSS.
+//   //   className: 'rob-lowe-icon',
+//   //   // Set marker width and height
+//   //   iconSize: [120, 181],
+//   //   // Set icon url
+//   //   iconUrl: 'assets/images/rob-lowe.png'
+//   // });
 
-  // /*
-  //  * Application Functions
-  //  */
-  // var addRobLoweIconOnClick = function(e) {
-  //   // Add this marker to map...
-  //   L.marker([e.latlng.lat, e.latlng.lng], {icon: robLoweIcon}).addTo(map);
-  // }
+//   // /*
+//   //  * Application Functions
+//   //  */
+//   // var addRobLoweIconOnClick = function(e) {
+//   //   // Add this marker to map...
+//   //   L.marker([e.latlng.lat, e.latlng.lng], {icon: robLoweIcon}).addTo(map);
+//   // }
 
-  /*
-   * Application Init
-   */
-  var map = L.mapbox.map('map', 'jmlabs.k3egm800', {
-    center: [40.55, -105.07], // lat, long
-    zoom: 13
-  });
+//   /*
+//    * Application Init
+//    */
+//   var map = L.mapbox.map('map', 'jmlabs.k3egm800', {
+//     center: [40.55, -105.07], // lat, long
+//     zoom: 13
+//   });
 
-  L.mapbox.tileLayer('mapbox.pencil', {
-      maxZoom: 18
-  }).addTo(map);
+//   L.mapbox.tileLayer('mapbox.geo', {
+//       maxZoom: 18
+//   }).addTo(map);
 
-  // // Add Rob Lowe Icons when Clicked!
-  // map.on('click', addRobLoweIconOnClick);
-});
+//   // // Add Rob Lowe Icons when Clicked!
+//   // map.on('click', addRobLoweIconOnClick);
+// });
